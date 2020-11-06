d9f5da8ddca9348e68b1c77ac27816f78d5f7e22
Jarsenal <jasper.zhou@gmail.com>
2020-11-06 18:26:24 +1100

    t

--- hello_app/views.py
+++ hello_app/views.py
@@ -26,3 +26,7 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+@app.route("/api/flowlog")
+def get_flowlog():
+    return app.send_static_file("PT1H.json")
